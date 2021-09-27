219198ece5642955c094f3715de893c20231be05
RdeHevia <55640085+RdeHevia@users.noreply.github.com>
2021-09-26 21:13:05 -0700

    v1

--- frontend/src/components/BinPage.js
+++ frontend/src/components/BinPage.js
@@ -20,10 +20,11 @@ const BinPage = ({ binId, handleGoToHome }) => {
       setCreationTime(new Date(bin.createdAt).toLocaleString("en-US", dateConfig));
       setLastUpdateTime(new Date(bin.updatedAt).toLocaleString("en-US", dateConfig));
       setEndPoint(`${window.location.origin}${bin.endPoint}`);
+    }).then(bin => {
       const sortedRequests = bin.requests.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
       setRequests(sortedRequests);
       setCurrentRequestId(sortedRequests[0]._id);
-    });
+    }).catch(err => console.log(err));
   }, []);
 
   const handleRefresh = async event => {
