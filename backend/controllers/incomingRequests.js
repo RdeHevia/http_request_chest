7002de1cd8f56219e48f6258dfb0ffc4edefb5ec
Raul de Hevia <raul.dehevia@gmail.com>
2022-01-02 13:04:55 +0100

    minor fix

--- backend/controllers/incomingRequests.js
+++ backend/controllers/incomingRequests.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { Request } = require('../models/requests');
-const Bin = require('../models/bins');
+const { Request } = require("../models/requests");
+const Bin = require("../models/bins");
 
 const parseRequest = (req) => {
   const ip = req.headers["x-forwarded-for"];
@@ -16,17 +16,21 @@ const parseRequest = (req) => {
   });
 };
 
-router.all('/:binId', async (req, res) => {
+router.all("/:binId", async (req, res) => {
   const bin = await Bin.findById(req.params.binId);
 
   if (bin) {
     const request = parseRequest(req);
     bin.requests.push(request);
     await bin.save();
-    res.json({message: `http-request successfully stored in Bin ${req.params.binId}`});
+    res.json({
+      message: `http-request successfully stored in Chest ${req.params.binId}`,
+    });
   } else {
     res.status(404);
-    res.json({message: `Bin with id ${req.params.binId} doesn't exists. Create one first`});
+    res.json({
+      message: `Chest with id ${req.params.binId} doesn't exists. Create one first`,
+    });
   }
 });
 
